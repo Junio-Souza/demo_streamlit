9bbb9bc1e3dea769072b064dc73c2bc28895ba48
Junio-Souza <147106144+Junio-Souza@users.noreply.github.com>
2026-02-06 12:01:18 -0300

    Update tela_2.py

--- pages/tela_2.py
+++ pages/tela_2.py
@@ -1,6 +1,22 @@
 import streamlit as st
 
-st.title("Tela 2")
-st.write("Conteúdo da segunda tela")
+st.title("Tela 2 – Resultado")
+
+# Proteção
+if "logado" not in st.session_state or not st.session_state.logado:
+    st.warning("Acesso não autorizado")
+    st.stop()
+
+if "dados" not in st.session_state:
+    st.info("Nenhum dado preenchido ainda")
+    st.stop()
+
+dados = st.session_state.dados
+
+st.subheader("Resumo dos dados informados")
+
+st.write(f"**Nome:** {dados['nome']}")
+st.write(f"**Idade:** {dados['idade']}")
+st.write(f"**Comentário:** {dados['comentario']}")
 
 st.success("Fluxo concluído com sucesso")
