6388232638f3f8907bfcc1eb8e63053a6c149fee
Junio-Souza <147106144+Junio-Souza@users.noreply.github.com>
2026-02-06 11:55:34 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Demos Web", layout="centered")
+
+st.title("Tela Inicial")
+st.write("Demo web em Python com 3 telas")
+
+st.info("Use o menu lateral para navegar")
